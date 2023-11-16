a2efcef6df392cc24cd94a8e72e2a055cb75b3d4
Bala190 <150993802+Bala190@users.noreply.github.com>
2023-11-15 21:33:45 -0800

    Add files via upload

---
+++ Abstraction/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+// Abstract class representing a shape
+public abstract class Shape
+{
+    // Abstract method for drawing
+    public abstract void Draw();
+}
+
+// Concrete class representing a circle
+public class Circle : Shape
+{
+    // Implementation of the Draw method for a circle
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle");
+    }
+}
+
+// Concrete class representing a square
+public class Square : Shape
+{
+    // Implementation of the Draw method for a square
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a square");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating instances of concrete classes
+        Shape myCircle = new Circle();
+        Shape mySquare = new Square();
+
+        // Using the Draw method without knowing the specific implementation
+        Console.WriteLine("Drawing shapes:");
+        DrawShape(myCircle);
+        DrawShape(mySquare);
+    }
+
+    // Method to draw a shape, accepting any class derived from Shape
+    static void DrawShape(Shape shape)
+    {
+        shape.Draw();
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class BankAccount
+{
+ 
+    private decimal balance;
+
+    
+    public void Deposit(decimal amount)
+    {
+        
+        balance += amount;
+        Console.WriteLine($"Deposited {amount:C}. New balance: {balance:C}");
+    }
+
+    
+    public void Withdraw(decimal amount)
+    {
+        
+        if (balance >= amount)
+        {
+            balance -= amount;
+            Console.WriteLine($"Withdrawn {amount:C}. New balance: {balance:C}");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds.");
+        }
+    }
+
+    
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+       
+        BankAccount myAccount = new BankAccount();
+
+        
+        myAccount.Deposit(1000);
+        myAccount.Withdraw(500);
+
+      
+        decimal currentBalance = myAccount.GetBalance();
+        Console.WriteLine($"Current balance: {currentBalance:C}");
+    }
+}
