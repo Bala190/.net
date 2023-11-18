55aac6b6ca3f68372896f37e488952044473724c
Bala190 <150993802+Bala190@users.noreply.github.com>
2023-11-17 21:22:35 -0800

    Add files via upload

---
+++ Using `finally` Block/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Simulating a situation where an exception might occur
+            int result = DivideNumbers(10, 0);
+            Console.WriteLine($"Result: {result}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            // Code in the finally block will always run, whether an exception occurred or not
+            Console.WriteLine("Finally block always executes, regardless of exceptions.");
+        }
+
+        // Rest of the program continues executing after handling the exception
+        Console.WriteLine("Program continues executing...");
+
+        Console.ReadLine();
+    }
+
+    static int DivideNumbers(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            // Throwing a DivideByZeroException
+            throw new DivideByZeroException("Cannot divide by zero!");
+        }
+
+        return numerator / denominator;
+    }
+}
+
